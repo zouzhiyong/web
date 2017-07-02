18fc896130ad5221eef7f18e20df93eeadac7a71
zouzhiyong <18616638@qq.com>
2017-07-02 19:23:40 +0800

    测试

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+  //配置参数
+  grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
+     concat: {
+         options: {
+             separator: ';',
+             stripBanners: true
+         },
+         dist: {
+             src: [
+                 "js/jquery.min.js",
+                 "js/bootstrap.min.js",
+                 "js/bootstrap-table.js",                 
+                 "js/bootstrap-table-zh-CN.min.js",
+                 "js/bootstrap-table-export.js",
+                 "js/vue.min.js",
+                 "js/index.js"
+             ],
+             dest: "js/default.js"
+         }
+     },
+     uglify: {
+         options: {
+         },
+         dist: {
+             files: {
+                 'assets/js/default.min.js': 'js/default.js'
+             }
+         }
+     },
+     cssmin: {
+         options: {
+             keepSpecialComments: 0
+         },
+         compress: {
+             files: {
+                 'assets/css/default.css': [
+                     "css/bootstrap.min.css",
+                     "css/bootstrap-table.css",                     
+                     "css/icon.css",
+                     "css/index.css"
+                 ]
+             }
+         }
+     }
+  });
+
+  //载入concat和uglify插件，分别对于合并和压缩
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  //注册任务
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+}
